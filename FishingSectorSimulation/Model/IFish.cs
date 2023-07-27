7ac0da8769a6ca0ff95b95457319d8080d2f5425
Shashwat Ratna <shashwatratna@gmail.com>
2023-07-27 22:11:02 +0530

    data added

---
+++ FishingSectorSimulation/Model/IFish.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishingSectorSimulation.Model
+{
+    public interface IFish
+    {
+        string type { get; set; }
+
+        int age { get; set; }
+
+        int lifespan { get; set; }
+
+
+    }
+}
